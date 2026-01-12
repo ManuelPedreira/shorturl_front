e04fc85564e0e86db5243fa3f5936893338d9719
Manuel <mapedgi@gmail.com>
2026-01-12 15:01:05 +0100

    i18n fix

--- src/ui/ErrorContainer/ErrorContainer.tsx
+++ src/ui/ErrorContainer/ErrorContainer.tsx
@@ -1,13 +1,25 @@
 import Link from "next/link";
 import styles from "./ErrorContainer.module.scss";
 
-const ErrorContainer = ({ children }: { children: React.ReactNode }) => {
+type ErrorContainerProps = {
+  title: string;
+  text: string;
+  description: string;
+  goBackButtonText: string;
+};
+
+const ErrorContainer = ({ title, text, description, goBackButtonText }: ErrorContainerProps) => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Something when wrong!</h1>
-      <div className={styles.message}>{children}</div>
+      <h1 className={styles.title}>{title}</h1>
+      <div className={styles.message}>
+        <p>{text}</p>
+        <small>{description}</small>
+      </div>
       <Link href={"/"} className={styles.link} aria-label="go-back-link">
-        <button className={styles.button} aria-label="go-back-button">Go Back</button>
+        <button className={styles.button} aria-label="go-back-button">
+          {goBackButtonText}
+        </button>
       </Link>
     </div>
   );
